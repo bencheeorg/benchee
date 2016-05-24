614619b6e223b9d316cedb544a83ceadf5fe239b
Tobias Pfeiffer <pragtob@gmail.com>
2016-05-24 09:04:16 +0200

    Move formatting to a special formatter

--- lib/benchee.ex
+++ lib/benchee.ex
@@ -57,47 +57,4 @@ defmodule Benchee do
     microseconds
   end
 
-  @label_width 30
-  @ips_width 15
-  @average_width 15
-  @deviation_width 15
-  @doc """
-  Creates a report of the benchmark suite run.
-  """
-  def report(%{jobs: jobs}) do
-    [column_descriptors | job_reports(jobs)]
-  end
-
-  defp column_descriptors do
-    "~*s~*s~*s~*s\n"
-    |> :io_lib.format([-@label_width, "Name", -@ips_width, "ips",
-                       -@average_width, "average", -@deviation_width, "deviation"])
-  end
-
-  defp job_reports(jobs) do
-    Enum.map jobs, fn(%{name: name, run_times: times}) ->
-      %{average:      average,
-        ips:          ips,
-        std_dev_ratio: std_dev_ratio} = Statistics.statistics(times)
-
-      "~*s~*.2f~*ts~*ts\n"
-      |> :io_lib.format([-@label_width, name, -@ips_width, ips,
-                         -@average_width, average_out(average),
-                         -@deviation_width, deviation_out(std_dev_ratio)])
-    end
-  end
-
-  defp average_out(average) do
-    "~.2f~ts"
-    |> :io_lib.format([average, "μs"])
-    |> to_string
-  end
-
-  defp deviation_out(std_dev_ratio) do
-    "(~ts~.2f%)"
-    |> :io_lib.format(["±", std_dev_ratio * 100.0])
-    |> to_string
-  end
-
-
 end
--- samples/simple_measure.exs
+++ samples/simple_measure.exs
@@ -1,4 +1,4 @@
 Benchee.init
 |> Benchee.benchmark("map", fn -> Enum.map(1..1_000, fn(i) -> i + 1 end) end)
-|> Benchee.report
+|> Benchee.Formatters.String.format
 |> IO.puts
