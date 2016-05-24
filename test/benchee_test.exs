adbb2336a54f4077e7ce3645b298b3137d633e28
Tobias Pfeiffer <tobi@bitcrowd.net>
2016-05-24 21:20:15 +0200

    integration level test

--- test/benchee_test.exs
+++ test/benchee_test.exs
@@ -1,4 +1,20 @@
 defmodule BencheeTest do
   use ExUnit.Case
+  import ExUnit.CaptureLog
   doctest Benchee
+
+  test "integration" do
+    capture_log fn ->
+      result =
+        Benchee.init(%{time: 0.1})
+        |> Benchee.benchmark("Sleeps", fn -> :timer.sleep(10) end)
+        |> Benchee.Statistics.statistics
+        |> Benchee.Formatters.String.format
+
+      [header, benchmark_stats] = result
+      assert Regex.match?(~r/Name.+ips.+average.+deviation/, header)
+
+      assert Regex.match?(~r/Sleeps/, benchmark_stats)
+    end
+  end
 end
