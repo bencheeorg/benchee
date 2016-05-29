2a299fd999b5d91627ae155c5410473eba7759a9
Tobias Pfeiffer <pragtob@gmail.com>
2016-05-29 14:04:13 +0200

    test to verify test run times are in order

--- test/benchee/benchmark_test.exs
+++ test/benchee/benchmark_test.exs
@@ -15,4 +15,15 @@ defmodule Benchee.BenchmarkTest do
       assert Enum.count(run_times) >= 9 # should be 10 but gotta give it a bit leeway
     end
   end
+
+  test ".benchmark doesn't take longer than advertised for very fast funs" do
+    capture_io fn ->
+      projected = 10_000
+      suite = %{config: %{time: projected}, jobs: []}
+      {time, _} = :timer.tc fn -> Benchee.benchmark(suite, "", fn -> 0 end) end
+
+      assert_in_delta projected, time, 500,
+                      "Benchmark excution took too long #{time} vs. #{projected}"
+    end
+  end
 end
