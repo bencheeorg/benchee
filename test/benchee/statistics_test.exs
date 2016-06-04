d65a62678e08b68ced01a390f75fee6f1c81a112
Tobias Pfeiffer <pragtob@gmail.com>
2016-06-04 16:49:22 +0200

    Extended stats tests

--- test/benchee/statistics_test.exs
+++ test/benchee/statistics_test.exs
@@ -2,6 +2,23 @@ defmodule Benchee.StatistcsTest do
   use ExUnit.Case
   doctest Benchee.Statistics
 
+  test "statistics computes the statistics for all jobs correctly" do
+    suite = %{jobs: [{"Job 1", [600, 470, 170, 430, 300]},
+                     {"Job 2", [17, 15, 23, 7, 9, 13]}]}
+
+    [{_, stats_1}, {_, stats_2}] = Benchee.Statistics.statistics suite
+
+    assert stats_1.average == 394
+    assert_in_delta stats_1.std_dev, 147.32, 0.01
+    assert_in_delta stats_1.std_dev_ratio, 0.37, 0.01
+    assert_in_delta stats_1.ips, 2538, 1
+
+    assert stats_2.average == 14
+    assert_in_delta stats_2.std_dev, 5.25, 0.01
+    assert_in_delta stats_2.std_dev_ratio, 0.37, 0.01
+    assert_in_delta stats_2.ips, 71428, 1
+  end
+
   test "sort sorts the benchmarks correctly and retains all date" do
     fourth = {"Fourth", %{average: 400.1, ips: 4_999.0,  std_dev_ratio: 0.7}}
     second = {"Second", %{average: 200.0, ips: 5_000.0,  std_dev_ratio: 2.1}}
