d3f0b3824961f8edc91eea97778445b6d1d024fe
Tobias Pfeiffer <tobi@bitcrowd.net>
2016-06-04 20:45:33 +0200

    Add MIT license

--- mix.exs
+++ mix.exs
@@ -7,25 +7,16 @@ defmodule Benchee.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     package: package,
+     name: "Benchee"
+   ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:mix_test_watch, "~> 0.2",      only: :dev},
@@ -33,5 +24,9 @@ defmodule Benchee.Mixfile do
       {:ex_doc,         "~> 0.11",     only: :dev},
       {:earmark,        "~> 0.2",       only: :dev}
     ]
+  end
+
+  defp package do
+    [licenses: ["MIT"]]
   end
 end
