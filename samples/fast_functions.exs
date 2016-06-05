f6b9540b2588420259da89cf58eceb7af0b12958
Tobias Pfeiffer <pragtob@gmail.com>
2016-06-05 13:31:28 +0200

    Benchmark noop along with fast_funs

--- samples/fast_functions.exs
+++ samples/fast_functions.exs
@@ -2,7 +2,8 @@ Benchee.run(%{time: 3},
              [{"Integer addition",          fn -> 1 + 1 end},
               {"String concatention",       fn -> "1" <> "1" end},
               {"adding a head to an array", fn -> [1 | [1]] end},
-              {"++ array concat",           fn -> [1] ++ [1] end}])
+              {"++ array concat",           fn -> [1] ++ [1] end},
+              {"noop",                      fn -> end}])
 
 
 # Before  adding running fast functions multiple times, these where just too
