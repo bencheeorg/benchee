758c4c0ad9534ef3a4bd5bf3ea8f7eb27fec492f
Tobias Pfeiffer <pragtob@gmail.com>
2016-06-05 19:13:02 +0200

    Should be more than sufficient to make a good 0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Benchee.Mixfile do
   def project do
     [
       app: :benchee,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
