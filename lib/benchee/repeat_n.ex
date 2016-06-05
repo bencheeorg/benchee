b3873d9976b098dff7dc49c60c6ef937271b386d
Tobias Pfeiffer <pragtob@gmail.com>
2016-06-05 12:11:44 +0200

    Include RepeatN in Benchee

---
+++ lib/benchee/repeat_n.ex
@@ -0,0 +1,34 @@
+defmodule Benchee.RepeatN do
+  @moduledoc """
+  Simple helper module that can easily make a function call repeat n times.
+  Which is significantly faster than Enum.each/list comprehension.
+
+  Check out the benchmark in samples/repeat_n.exs:
+
+  Name                          ips            average        deviation      median
+  Recursion                     76037.28       13.15μs        (±10.76%)      13.0μs
+  Enum.each                     54930.00       18.20μs        (±22.01%)      18.0μs
+  List comprehension            46742.24       21.39μs        (±20.72%)      21.0μs
+
+  Comparison:
+  Recursion                     76037.28
+  Enum.each                     54930.00        - 1.38x slower
+  List comprehension            46742.24        - 1.63x slower
+  """
+
+  @doc """
+  Calls the given function n times.
+  """
+  def repeat_n(function, 0) do
+    # noop
+  end
+
+  def repeat_n(function, 1) do
+    function.()
+  end
+
+  def repeat_n(function, count) do
+    function.()
+    repeat_n(function, count - 1)
+  end
+end
---
+++ test/benchee/repeat_n_test.exs
@@ -0,0 +1,29 @@
+defmodule Benchee.RepeatNTest do
+  use ExUnit.Case
+  import Benchee.RepeatN
+  import ExUnit.CaptureIO
+
+  test "calls it n times" do
+    assert_called_n 10
+  end
+
+  test "calls it only one time when 1 is specified" do
+    assert_called_n 1
+  end
+
+  test "calls it 0 times when 0 is specified" do
+    assert_called_n 0
+  end
+
+  defp assert_called_n(n) do
+    output = capture_io fn ->
+      repeat_n(fn -> IO.write "1" end, n)
+    end
+
+    assert times_called(output) == n
+  end
+
+  defp times_called(output) do
+    String.length output
+  end
+end
