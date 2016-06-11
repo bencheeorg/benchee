3a886241169ac65d2cad52671eff401b6fd57a33
Tobias Pfeiffer <pragtob@gmail.com>
2016-06-11 09:41:56 +0200

    reworked multiple map benchmark

--- samples/multiple_map.exs
+++ samples/multiple_map.exs
@@ -1,42 +1,73 @@
 defmodule MyMap do
 
-  def map(list, function) do
-    Enum.reverse _map([], list, function)
+  def map_tco(list, function) do
+    Enum.reverse _map_tco([], list, function)
   end
 
-  defp _map(acc, [head | tail], function) do
-    _map([function.(head) | acc], tail, function)
+  defp _map_tco(acc, [head | tail], function) do
+    _map_tco([function.(head) | acc], tail, function)
   end
 
-  defp _map(acc, [], _function) do
+  defp _map_tco(acc, [], _function) do
     acc
   end
 
-  def map2(acc \\ [], list, function)
+  def map_tco_concat(acc \\ [], list, function)
 
-  def map2(acc, [head | tail], function) do
-    map2(acc ++ [function.(head)], tail, function)
+  def map_tco_concat(acc, [head | tail], function) do
+    map_tco_concat(acc ++ [function.(head)], tail, function)
   end
 
-  def map2(acc, [], _function) do
+  def map_tco_concat(acc, [], _function) do
     acc
   end
 
-  def map3([], _func), do: []
+  def map_body([], _func), do: []
 
-  def map3([head | tail], func) do
-    [func.(head) | map(tail, func)]
+  def map_body([head | tail], func) do
+    [func.(head) | map_body(tail, func)]
+  end
+
+  def map_tco_no_reverse(list, function) do
+    _map_tco([], list, function)
   end
 end
 
 
 list = Enum.to_list(1..1_000)
 map_function = fn(i) -> i + 1 end
-Benchee.init(%{time: 3})
+Benchee.init
 |> Benchee.benchmark("stdlib map", fn -> Enum.map(list, map_function) end)
-|> Benchee.benchmark("map with TCO and reverse", fn -> MyMap.map(list, map_function) end)
-|> Benchee.benchmark("map with ++ and TCO", fn -> MyMap.map2(list, map_function) end)
-|> Benchee.benchmark("simple map without TCO", fn -> MyMap.map3(list, map_function) end)
+|> Benchee.benchmark("map with TCO and reverse",
+                     fn -> MyMap.map_tco(list, map_function) end)
+|> Benchee.benchmark("map with TCO and ++",
+                     fn -> MyMap.map_tco_concat(list, map_function) end)
+|> Benchee.benchmark("bodyrecusrive map",
+                     fn -> MyMap.map_body(list, map_function) end)
+|> Benchee.benchmark("map tco no reverse",
+                     fn -> MyMap.map_tco_no_reverse(list, map_function) end)
 |> Benchee.Statistics.statistics
 |> Benchee.Formatters.Console.format
 |> IO.puts
+
+
+tobi@happy ~/github/benchee $ mix run samples/multiple_map.exs
+Benchmarking stdlib map...
+Benchmarking map with TCO and reverse...
+Benchmarking map with TCO and ++...
+Benchmarking bodyrecusrive map...
+Benchmarking map tco no reverse...
+
+# Name                          ips            average        deviation      median
+# bodyrecusrive map             65854.74       15.18μs        (±31.83%)      15.00μs
+# stdlib map                    65594.56       15.25μs        (±30.44%)      15.00μs
+# map tco no reverse            58534.15       17.08μs        (±39.71%)      17.00μs
+# map with TCO and reverse      55066.15       18.16μs        (±39.89%)      18.00μs
+# map with TCO and ++           739.36         1352.52μs      (±2.60%)       1351.00μs
+#
+# Comparison:
+# bodyrecusrive map             65854.74
+# stdlib map                    65594.56        - 1.00x slower
+# map tco no reverse            58534.15        - 1.13x slower
+# map with TCO and reverse      55066.15        - 1.20x slower
+# map with TCO and ++           739.36          - 89.07x slower
