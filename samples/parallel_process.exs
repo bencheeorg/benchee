80b7a523e28e5d25784a9b2a930fd499c546c4a8
Tobias Pfeiffer <pragtob@gmail.com>
2016-07-09 10:03:49 +0200

    enhance parallel_processes sample with multiple runs and no iex

--- samples/parallel_process.exs
+++ samples/parallel_process.exs
@@ -1,16 +1,20 @@
 # When passing a flag parallel with value >1 then multiple processes
 # will be handled for benchmarking provided function.
 
-Benchee.run %{parallel: 5, time: 10}, [{"five", fn -> :timer.sleep(1000) end}]
+Benchee.run %{parallel: 1, time: 10},[{"one", fn -> :timer.sleep(1000) end}]
+Benchee.run %{parallel: 3, time: 10},[{"three", fn -> :timer.sleep(1000) end}]
+Benchee.run %{parallel: 5, time: 10},[{"five", fn -> :timer.sleep(1000) end}]
 
-#iex(1)> Benchee.run %{time: 10}, [{"one", fn -> :timer.sleep(1000) end}]
-#Benchmarking name: one, parallel: 1, time: 10.0, warmup: 2.0...
+# tobi@happy ~/github/benchee $ mix run samples/parallel_process.exs 
+# Benchmarking one...
 #
-#Name           ips        average    deviation         median
-#one             1.00   1004495.78μs     (±0.15%)   1005146.00μs
-
-#iex(2)> Benchee.run %{parallel: 5, time: 10}, [{"five", fn -> :timer.sleep(1000) end}]
-#Benchmarking name: five, parallel: 5, time: 10.0, warmup: 2.0...
+# Name           ips        average    deviation         median
+# one           1.00   1000991.78μs     (±0.00%)   1000994.00μs
+# Benchmarking three...
 #
-#Name           ips        average    deviation         median
-#five            4.98   1003737.24μs     (±0.10%)   1003672.00μs
+# Name            ips        average    deviation         median
+# three          1.00   1000998.63μs     (±0.00%)   1000998.00μs
+# Benchmarking five...
+#
+# Name           ips        average    deviation         median
+# five          1.00   1000996.24μs     (±0.00%)   1000996.00μs
