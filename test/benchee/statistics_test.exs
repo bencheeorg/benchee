efb5458fe2bd609d368cf44dc7f0a90f3136cc8c
Tobias Pfeiffer <pragtob@gmail.com>
2016-07-10 13:27:33 +0200

    Adjust Statistics.sort test to rightfully make it work with Map

--- test/benchee/statistics_test.exs
+++ test/benchee/statistics_test.exs
@@ -32,7 +32,7 @@ defmodule Benchee.StatistcsTest do
     second = {"Second", %{average: 200.0, ips: 5_000.0,  std_dev_ratio: 2.1}}
     third  = {"Third",  %{average: 400.0, ips: 2_500.0,  std_dev_ratio: 1.5}}
     first  = {"First",  %{average: 100.0, ips: 10_000.0, std_dev_ratio: 0.3}}
-    jobs = [fourth, second, third, first]
+    jobs = Map.new [fourth, second, third, first]
 
     assert Benchee.Statistics.sort(jobs) == [first, second, third, fourth]
   end
