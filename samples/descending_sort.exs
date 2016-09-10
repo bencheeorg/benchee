ef6e365c1271e069ae6786d1db588e79076572e5
Tobias Pfeiffer <tobi@bitcrowd.net>
2016-09-10 08:25:57 +0200

    Some benchmark updates
    Gues I'll drop those or somehow automate them in the future,
    tedious process :|

--- samples/descending_sort.exs
+++ samples/descending_sort.exs
@@ -4,3 +4,26 @@ Benchee.run %{
   "sort |> reverse"  => fn -> list |> Enum.sort |> Enum.reverse  end,
   "sort(fun)"        => fn -> Enum.sort(list, &(&1 > &2)) end,
   "sort_by(-value)"  => fn -> Enum.sort_by(list, fn(val) -> -val end) end}
+
+# tobi@airship ~/github/benchee $ mix run samples/descending_sort.exs
+# Erlang/OTP 19 [erts-8.0] [source] [64-bit] [smp:8:8] [async-threads:10] [hipe] [kernel-poll:false]
+# Elixir 1.3.2
+# Benchmark suite executing with the following configuration:
+# warmup: 2.0s
+# time: 5.0s
+# parallel: 1
+# Estimated total run time: 21.0s
+#
+# Benchmarking sort |> reverse...
+# Benchmarking sort(fun)...
+# Benchmarking sort_by(-value)...
+#
+# Name                      ips        average    deviation         median
+# sort |> reverse        587.92     1700.91 μs     (±6.39%)     1671.00 μs
+# sort(fun)              229.59     4355.65 μs     (±3.20%)     4321.00 μs
+# sort_by(-value)        146.59     6821.76 μs     (±4.18%)     6724.00 μs
+#
+# Comparison:
+# sort |> reverse        587.92
+# sort(fun)              229.59 - 2.56x slower
+# sort_by(-value)        146.59 - 4.01x slower
--- samples/repat_n.exs
+++ samples/repat_n.exs
@@ -12,6 +12,8 @@ Benchee.run %{
 }
 
 # tobi@happy ~/github/benchee $ mix run samples/repat_n.exs
+# Erlang/OTP 19 [erts-8.0] [source] [64-bit] [smp:8:8] [async-threads:10] [hipe] [kernel-poll:false]
+# Elixir 1.3.2
 # Benchmark suite executing with the following configuration:
 # warmup: 2.0s
 # time: 5.0s
@@ -25,15 +27,15 @@ Benchee.run %{
 # Benchmarking Recursion...
 #
 # Name                                 ips        average    deviation         median
-# Recursion                       89641.19        11.16μs    (±22.51%)        11.00μs
-# Enum.each (list)                49061.60        20.38μs    (±21.32%)        20.00μs
-# List comprehension (list)       44405.33        22.52μs    (±30.45%)        22.00μs
-# List comprehension (range)      20914.16        47.81μs    (±11.76%)        47.00μs
-# Enum.each (range)               19924.18        50.19μs     (±4.12%)        50.00μs
+# Recursion                       68829.80       14.53 μs    (±15.57%)       14.00 μs
+# Enum.each (list)                40328.88       24.80 μs    (±25.80%)       24.00 μs
+# List comprehension (list)       34124.09       29.30 μs    (±16.76%)       28.00 μs
+# List comprehension (range)      16241.21       61.57 μs    (±11.46%)       60.00 μs
+# Enum.each (range)               15143.96       66.03 μs    (±11.82%)       64.00 μs
 #
 # Comparison:
-# Recursion                       89641.19
-# Enum.each (list)                49061.60 - 1.83x slower
-# List comprehension (list)       44405.33 - 2.02x slower
-# List comprehension (range)      20914.16 - 4.29x slower
-# Enum.each (range)               19924.18 - 4.50x slower
+# Recursion                       68829.80
+# Enum.each (list)                40328.88 - 1.71x slower
+# List comprehension (list)       34124.09 - 2.02x slower
+# List comprehension (range)      16241.21 - 4.24x slower
+# Enum.each (range)               15143.96 - 4.55x slower
