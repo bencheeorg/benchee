6d3a286d018144e1481fb5d30412f6ff7827b2cf
Tobias Pfeiffer <pragtob@gmail.com>
2016-10-11 09:29:50 +0200

    Benchee.Conversion.Unit moduledoc

--- lib/benchee/conversion/unit.ex
+++ lib/benchee/conversion/unit.ex
@@ -1,4 +1,17 @@
 defmodule Benchee.Conversion.Unit do
+  @moduledoc """
+  A representation of the different units used in `Benchee.Conversion.Duration`
+  and `Benchee.Conversion.Count`. A unit is characterized by:
+
+  * name - an atom representation of the unit for easy access (`:microseconds`,
+  `thousand`)
+  * magnitude - compared to he base unit (the smallest unit) what's the factor
+  you had to multiply it by to get back to the base unit. E.g. the thousand
+  unit has a magnitude of `1_000`.
+  * label - a string that is used as a unit label (`"K"` for a thousand f.ex.)
+  * long - a string giving the long version of the label (`"Thousand"`)
+  """
+
   defstruct [:name, :magnitude, :label, :long]
   @type t :: %Benchee.Conversion.Unit{name:      atom,
                                       magnitude: non_neg_integer,
