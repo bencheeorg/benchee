cc1741d3f592a9f791b8314893873e1d0c693962
Tobias Pfeiffer <pragtob@gmail.com>
2016-11-30 13:55:14 +0100

    Phew.. glad I got samples - fix to_map with top level empty list

--- lib/benchee/utility/deep_convert.ex
+++ lib/benchee/utility/deep_convert.ex
@@ -17,12 +17,18 @@ defmodule Benchee.Utility.DeepConvert do
 
   iex> Benchee.Utility.DeepConvert.to_map(%{a: %{b: 2}, c: %{d: 3, e: 5}})
   %{a: %{b: 2}, c: %{d: 3, e: 5}}
+
+  iex> Benchee.Utility.DeepConvert.to_map([])
+  %{}
   """
-  def to_map(kwlist = [{_key, _value} | _tail]) do
+  def to_map([]), do: %{}
+  def to_map(structure), do: do_to_map(structure)
+
+  defp do_to_map(kwlist = [{_key, _value} | _tail]) do
     kwlist
-    |> Enum.map(fn({key, value}) -> {key, to_map(value)} end)
+    |> Enum.map(fn({key, value}) -> {key, do_to_map(value)} end)
     |> Map.new
   end
-  def to_map(no_list), do: no_list
+  defp do_to_map(no_list), do: no_list
 
 end
