3df0a15db1b3826c8bc725754cbe53040110d3c8
Tobias Pfeiffer <pragtob@gmail.com>
2017-04-25 23:13:21 +0200

    Drop elixir 1.2 support
    * dialyzer on travis was failing with elixir 1.2
    * plus see stuff mentioned in #30

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Benchee.Mixfile do
     [
       app: :benchee,
       version: @version,
-      elixir: "~> 1.2",
+      elixir: "~> 1.3",
       elixirc_paths: elixirc_paths(Mix.env),
       consolidate_protocols: true,
       build_embedded: Mix.env == :prod,
