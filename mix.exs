4efa4b5056c1c89bf4d50fc394d5e4a4c7f1e4af
Tobias Pfeiffer <pragtob@gmail.com>
2019-02-10 10:34:45 +0100

    updates

--- mix.exs
+++ mix.exs
@@ -52,7 +52,7 @@ defmodule Benchee.Mixfile do
       {:deep_merge, "~> 0.1"},
       {:ex_guard, "~> 1.3", only: :dev},
       {:credo, "~> 1.0.0", only: :dev},
-      {:ex_doc, "~> 0.18.0", only: :dev},
+      {:ex_doc, "~> 0.19.0", only: :dev},
       {:earmark, "~> 1.0", only: :dev},
       {:excoveralls, "~> 0.7", only: :test},
       {:inch_ex, "~> 0.5", only: :docs},
