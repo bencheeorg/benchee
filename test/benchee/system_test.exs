dd2c2bf9038403e888841d741f632ae7aba723e1
Tobias Pfeiffer <pragtob@gmail.com>
2019-03-27 11:43:29 +0100

    Minimize System Interface by adjusting tests
    When moving all this into a new library all of these will be
    public there.

--- test/benchee/system_test.exs
+++ test/benchee/system_test.exs
@@ -3,9 +3,10 @@ defmodule Benchee.SystemTest do
 
   alias Benchee.Suite
   import ExUnit.CaptureIO
+  import Benchee.System
 
   test ".system adds the content to a given suite" do
-    system_info = Benchee.System.system(%Suite{})
+    system_info = system(%Suite{})
 
     assert %{
              system: %{
@@ -20,26 +21,26 @@ defmodule Benchee.SystemTest do
   end
 
   test ".elixir returns the current elixir version" do
-    assert Benchee.System.elixir() =~ ~r/\d+\.\d+\.\d+/
+    assert system(%Suite{}).system.elixir =~ ~r/\d+\.\d+\.\d+/
   end
 
   test ".erlang returns the current erlang version in use" do
-    version = Benchee.System.erlang()
+    version = system(%Suite{}).system.erlang
     assert version =~ to_string(:erlang.system_info(:otp_release))
     assert version =~ ~r/\d+\.\d+/
     refute version =~ "\n"
   end
 
   test ".num_cores returns the number of cores on the running VM" do
-    assert Benchee.System.num_cores() > 0
+    assert system(%Suite{}).system.num_cores > 0
   end
 
   test ".os returns an atom of the current os" do
-    assert Enum.member?([:Linux, :FreeBSD, :macOS, :Windows], Benchee.System.os())
+    assert Enum.member?([:Linux, :FreeBSD, :macOS, :Windows], system(%Suite{}).system.os)
   end
 
   test ".cpu_speed returns a string (more accurate tests in .parse_cpu_for)" do
-    assert "" <> _actual_content = Benchee.System.cpu_speed()
+    assert "" <> _actual_content = system(%Suite{}).system.cpu_speed
   end
 
   describe ".parse_cpu_for" do
@@ -52,25 +53,25 @@ defmodule Benchee.SystemTest do
     stepping	: 3
     """
     test "for :Linux handles some normal intel output" do
-      output = Benchee.System.parse_cpu_for(:Linux, @cat_proc_cpu_info_excerpt)
+      output = parse_cpu_for(:Linux, @cat_proc_cpu_info_excerpt)
       assert output =~ "Intel(R) Core(TM) i7-4790 CPU @ 3.60GHz"
     end
 
     test "for :Linux handles Semaphore CI output" do
       semaphore_output = "model name	: Intel Core Processor (Haswell)"
-      output = Benchee.System.parse_cpu_for(:Linux, semaphore_output)
+      output = parse_cpu_for(:Linux, semaphore_output)
       assert output =~ "Haswell"
     end
 
     test "for :Linux handles unknown architectures" do
       raw_output = "Bender Bending Rodriguez"
-      output = Benchee.System.parse_cpu_for(:Linux, raw_output)
+      output = parse_cpu_for(:Linux, raw_output)
       assert output == "Unrecognized processor"
     end
   end
 
   test ".available_memory returns the available memory on the computer" do
-    {num, rest} = Float.parse(Benchee.System.available_memory())
+    {num, rest} = Float.parse(system(%Suite{}).system.available_memory)
 
     decimal_part_of_available_memory =
       "#{num}"
@@ -88,14 +89,14 @@ defmodule Benchee.SystemTest do
 
     captured_io =
       capture_io(fn ->
-        Benchee.System.system_cmd("cat", "dev/null", system_func)
+        system_cmd("cat", "dev/null", system_func)
       end)
 
     assert captured_io =~ "Something went wrong"
     assert captured_io =~ "ERROR"
 
     capture_io(fn ->
-      assert Benchee.System.system_cmd("cat", "dev/null", system_func) == "N/A"
+      assert system_cmd("cat", "dev/null", system_func) == "N/A"
     end)
   end
 end
