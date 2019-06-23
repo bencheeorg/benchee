faf1d87192fc9121781222a1b39bffa5e8729a21
Tobias Pfeiffer <pragtob@gmail.com>
2019-06-23 15:09:19 +0200

    Failing percentile property
    No Internet atm to research what correct behaviour even looks
    like it.

--- test/statistex_test.exs
+++ test/statistex_test.exs
@@ -6,36 +6,57 @@ defmodule Statistex.StatistexTest do
   import Statistex
   import StreamData
 
-  property "doesn't blow up no matter what kind of nonempty list of floats it's given" do
-    check all samples <- list_of(float(), min_length: 1) do
-      stats = statistics(samples)
+  describe "property testing as we might get loads of data" do
+    property "doesn't blow up no matter what kind of nonempty list of floats it's given" do
+      check all samples <- list_of(float(), min_length: 1) do
+        stats = statistics(samples)
 
-      assert stats.sample_size >= 1
-      assert stats.minimum <= stats.maximum
+        assert stats.sample_size >= 1
+        assert stats.minimum <= stats.maximum
 
-      assert stats.minimum <= stats.average
-      assert stats.average <= stats.maximum
+        assert stats.minimum <= stats.average
+        assert stats.average <= stats.maximum
 
-      assert stats.minimum <= stats.median
-      assert stats.median <= stats.maximum
+        assert stats.minimum <= stats.median
+        assert stats.median <= stats.maximum
 
-      assert stats.median == stats.percentiles[50]
+        assert stats.median == stats.percentiles[50]
 
-      assert stats.standard_deviation >= 0
-      assert stats.standard_deviation_ratio >= 0
+        assert stats.standard_deviation >= 0
+        assert stats.standard_deviation_ratio >= 0
 
-      case stats.mode do
-        [_ | _] ->
-          Enum.each(stats.mode, fn mode ->
-            assert(mode in samples)
-          end)
+        # mode actually occurs in the samples
+        case stats.mode do
+          [_ | _] ->
+            Enum.each(stats.mode, fn mode ->
+              assert(mode in samples)
+            end)
 
-        # nothing to do there is no real mode
-        nil ->
-          nil
+          # nothing to do there is no real mode
+          nil ->
+            nil
 
-        mode ->
-          assert mode in samples
+          mode ->
+            assert mode in samples
+        end
+      end
+    end
+
+    property "percentiles are correctly related to each other" do
+      pending("""
+      seems like for 2 element lists the 50th percentiles ends up
+      smaller than the 25th percentile which definitely isn't cool.
+
+      I have no Internet atm to research what the correct behaviour should be.
+      """)
+
+      check all samples <- list_of(float(), min_length: 1) do
+        percies = percentiles(samples, [25, 50, 75, 90, 99])
+
+        assert percies[25] <= percies[50]
+        assert percies[50] <= percies[75]
+        assert percies[75] <= percies[90]
+        assert percies[90] <= percies[99]
       end
     end
   end
