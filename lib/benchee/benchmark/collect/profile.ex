7ec1da3a6936fec2f6d92d74084be0b2d3813735
Tobias Pfeiffer <pragtob@gmail.com>
2022-02-20 12:50:38 +0100

    First shot at making profilers work with inputs and hooks
    Basically reusing the same code we used for pre_check.
    
    However, there is the problem where after_each won't work that
    still needs fixing but probably after it's fixed upstream in
    elixir core as I don't want to implement all of this:
    
    https://groups.google.com/g/elixir-lang-core/c/f0gP0It-yuU

---
+++ lib/benchee/benchmark/collect/profile.ex
@@ -0,0 +1,21 @@
+defmodule Benchee.Benchmark.Collect.Profile do
+  @moduledoc """
+  Collect/generate a profile.
+
+  The most unusual collector, as it doesn't return measured values really.
+
+  It still goes around and gathers data about a passed in function and so made sense
+  to put it into similar confines.
+  """
+  @default_profiler Benchee.Profile.default_profiler()
+  def collect(function, opts \\ [profiler_module: @default_profiler, profiler_opts: []]) do
+    {:ok, profiler_module} = Access.fetch(opts, :profiler_module)
+    profiler_opts = Access.get(opts, :profiler_opts, [])
+
+    # note, I already put this here but it doesn't work yet
+    # see elixir core discussion: https://groups.google.com/g/elixir-lang-core/c/f0gP0It-yuU
+    return_value = profiler_module.profile(function, profiler_opts)
+
+    {nil, return_value}
+  end
+end
