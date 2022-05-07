1de81d2b656fb03b40641d81092e0794f60e3d6a
Brooklin <brooklinjazzmyers@gmail.com>
2022-05-07 13:15:57 -0700

    remove added whitespace

--- lib/benchee.ex
+++ lib/benchee.ex
@@ -40,7 +40,6 @@ for {module, moduledoc} <- [{Benchee, elixir_doc}, {:benchee, erlang_doc}] do
         )
     """
     @spec run(map, keyword) :: any
-
     def run(jobs, config \\ []) when is_list(config) do
       config
       |> Benchee.init()
@@ -57,7 +56,11 @@ for {module, moduledoc} <- [{Benchee, elixir_doc}, {:benchee, erlang_doc}] do
     defp add_benchmarking_jobs(suite, jobs) do
       Enum.reduce(jobs, suite, fn {key, function}, suite_acc ->
         if :erlang.fun_info(function, :module) == {:module, :erl_eval} do
-          IO.puts("warning: the benchmark #{key} is using an evaluated function. Evaluated functions perform slower than compiled functions. Consider moving all of the Benchee caller to a function in a module and invoking the `Mod.fun()` instead.")
+          IO.puts("""
+          Warning: the benchmark #{key} is using an evaluated function.
+            Evaluated functions perform slower than compiled functions.
+            Consider moving all of the Benchee caller to a function in a module and invoking the `Mod.fun()` instead.
+          """)
         end
 
         Benchee.benchmark(suite_acc, key, function)
