24dc91e62b85fcdee07d5862e0b9ecd9d921c360
Tobias Pfeiffer <pragtob@gmail.com>
2023-11-08 12:39:53 +0100

    Adapt some values to fight flakyness

--- test/benchee/benchmark/collect/reductions_test.exs
+++ test/benchee/benchmark/collect/reductions_test.exs
@@ -7,8 +7,10 @@ defmodule Benchee.Collect.ReductionsTest do
       assert {reductions, [1, 2, 3, 4, 5, 6, 7, 8, 9]} =
                Reductions.collect(fn -> Enum.to_list(1..9) end)
 
+      # it seems that the JIT may or may not interfere here with the values as we get flakyness
+      # on higher OTP versions
       assert reductions >= 13
-      assert reductions <= 58
+      assert reductions <= 71
 
       assert {reductions, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} =
                Reductions.collect(fn -> Enum.to_list(1..10) end)
