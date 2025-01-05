40af6177747603c97a0e848e67ce325b2d449606
Tobias Pfeiffer <pragtob@gmail.com>
2025-01-05 16:32:12 +0100

    Add a pre-check sample

---
+++ samples/pre_check.exs
@@ -0,0 +1,4 @@
+a = 7
+Benchee.run(%{"a*2" => fn -> a * 2 end, "a+a" => fn -> a + a end}, time: 0, warmup: 0, pre_check: true)
+Benchee.run(%{"a*2" => fn -> a * 2 end, "a+a" => fn -> a + a end}, time: 0, warmup: 0, pre_check: :all_same)
+Benchee.run(%{"a*2" => fn -> a * 2 end, "a+a" => fn -> a + a + 1 end}, time: 0, warmup: 0, pre_check: :all_same)
