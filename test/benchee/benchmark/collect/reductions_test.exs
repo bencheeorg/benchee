83228f283fdc6f6e7faebb55f0973ec4a270cfa2
Tobias Pfeiffer <pragtob@gmail.com>
2025-01-12 16:21:37 +0100

    Loosen up reduction limits due to continued flakyness

--- test/benchee/benchmark/collect/reductions_test.exs
+++ test/benchee/benchmark/collect/reductions_test.exs
@@ -7,22 +7,22 @@ defmodule Benchee.Collect.ReductionsTest do
       assert {reductions, [1, 2, 3, 4, 5, 6, 7, 8, 9]} =
                Reductions.collect(fn -> Enum.to_list(1..9) end)
 
-      # it seems that the JIT may or may not interfere here with the values as we get flakyness
-      # on higher OTP versions
+      # it seems that the JIT may interfere here with the values as we get flakyness
+      # on higher OTP versions and so we're loosening up the boundaries here, sadly
       assert reductions >= 13
-      assert reductions <= 71
+      assert reductions <= 250
 
       assert {reductions, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} =
                Reductions.collect(fn -> Enum.to_list(1..10) end)
 
       assert reductions >= 14
-      assert reductions <= 61
+      assert reductions <= 250
 
       assert {reductions, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]} =
                Reductions.collect(fn -> Enum.to_list(1..11) end)
 
       assert reductions >= 14
-      assert reductions <= 64
+      assert reductions <= 250
     end
   end
 end
