d0c148b38b4a81ec481347e47732ab1138bc166f
Tobias Pfeiffer <pragtob@gmail.com>
2025-04-14 21:44:57 +0200

    make the pre check sample not fail the samples run

--- samples/pre_check.exs
+++ samples/pre_check.exs
@@ -12,8 +12,9 @@ Benchee.run(%{"a*2" => fn -> a * 2 end, "a+a" => fn -> a + a end},
   pre_check: :all_same
 )
 
-Benchee.run(%{"a*2" => fn -> a * 2 end, "a+a" => fn -> a + a + 1 end},
-  time: 0,
-  warmup: 0,
-  pre_check: :all_same
-)
+# this one is expected to fail, commented out by default so that `run_samples.sh` doesn't quit here
+# Benchee.run(%{"a*2" => fn -> a * 2 end, "a+a (wrong)" => fn -> a + a + 1 end},
+#   time: 0,
+#   warmup: 0,
+#   pre_check: :all_same
+# )
