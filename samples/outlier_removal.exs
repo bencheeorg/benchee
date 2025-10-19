7d24851d7078f8a2431164dd23e5be3a70d430a3
Tobias Pfeiffer <pragtob@gmail.com>
2025-10-19 14:33:51 +0200

    adjust test script to have all the output and more readable

--- samples/outlier_removal.exs
+++ samples/outlier_removal.exs
@@ -8,14 +8,16 @@ suite =
       "map.flatten" => fn -> list |> Enum.map(map_fun) |> List.flatten() end
     },
     formatters: [{Benchee.Formatters.Console, extended_statistics: true}],
-    exclude_outliers: true
+    exclude_outliers: true,
+    warmup: 0,
+    time: 1
   )
 
 suite.scenarios
 |> Enum.map(fn scenario ->
   statistics = scenario.run_time_data.statistics
 
-  {scenario.name, length(statistics.outliers), statistics.outliers,
-   statistics.lower_outlier_bound, statistics.upper_outlier_bound}
+  {scenario.name, length(statistics.outliers), statistics.lower_outlier_bound,
+   statistics.upper_outlier_bound, statistics.outliers}
 end)
-|> IO.inspect()
+|> IO.inspect(printable_limit: :infinity, limit: :infinity)
