bdbdeb00058dfd5ef9ff29f34dcada702fdc7d65
Tobias Pfeiffer <pragtob@gmail.com>
2025-10-21 21:43:02 +0200

    Exclude tests from duplicated code check
    Sometimes tests are a bit repeated, and usually that is fine.

--- .credo.exs
+++ .credo.exs
@@ -71,7 +71,7 @@
         # or the `schema` macro in Ecto schemas to trigger DuplicatedCode, just
         # set the `excluded_macros` parameter to `[:schema, :setup, :test]`.
         #
-        {Credo.Check.Design.DuplicatedCode, excluded_macros: []},
+        {Credo.Check.Design.DuplicatedCode, excluded_macros: [], files: %{excluded: "test/**/*_test.exs"}},
         # You can also customize the exit_status of each check.
         # If you don't want TODO comments to cause `mix credo` to fail, just
         # set this value to 0 (zero).
