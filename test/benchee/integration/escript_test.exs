e0302ac62ff30527975d13791c0ef239b4b43eab
Tobias Pfeiffer <pragtob@gmail.com>
2026-06-06 12:12:18 +0200

    Put integration tests into a new integration folder and explain what it is there for

--- test/benchee/integration/escript_test.exs
+++ test/benchee/integration/escript_test.exs
@@ -1,11 +1,12 @@
 defmodule Benchee.EscriptTest do
+  # Cding is global, so can't run in parallel
   use ExUnit.Case, async: false
 
   import Benchee.IntegrationHelpers
 
   describe "escript building" do
     @working_directory File.cwd!()
-    @sample_project_directory Path.expand("../fixtures/escript", __DIR__)
+    @sample_project_directory Path.expand("../../fixtures/escript", __DIR__)
     test "benchee can be built into and used as an escript" do
       File.cd!(@sample_project_directory)
       # we don't match the exit_status right now to get better error messages potentially
